55c1896035ff853b387d2e2f09ce1da9a2c6f3f8
fadeomar <fadeomar2015@gmail.com>
2020-04-03 21:00:36 +0300

    istall regenerator-runtime becuse babel give error for async await

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
+import regeneratorRuntime from 'regenerator-runtime';
 import models from '../models';
+
 import { hashPassword, jwtToken, comparePassword } from '../utils';
 
 const { User } = models;
@@ -12,21 +15,6 @@ const auth = {
       const token = jwtToken.createToken(user);
       const { id } = user;
       return res.status(201).send({ token, user: { id, name, email } });
-    } catch (e) {
-      return next(new Error(e));
-    }
-  },
-
-  async signIn(req, res, next) {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ where: { email } });
-      if (user && comparePassword(password, user.password)) {
-        const { name, id } = user;
-        const token = jwtToken.createToken(user);
-        return res.status(200).send({ token, user: { id, name, email } });
-      }
-      return res.status(400).send({ error: 'invalid email/password combination ' });
     } catch (e) {
       return next(new Error(e));
     }
