948c752e7fc157089220a1ca8b283f5bc574b86c
fadeomar <fadeomar2015@gmail.com>
2020-04-03 23:47:29 +0300

    create todo's and todoItems table and associated it with the user table

---
+++ api/models/todoitem.js
@@ -0,0 +1,16 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const TodoItem = sequelize.define('TodoItem', {
+    text: DataTypes.STRING,
+    todoId: DataTypes.INTEGER,
+    isCompleted: DataTypes.BOOLEAN
+  }, {});
+  TodoItem.associate = (models) => {
+    // associations can be defined here
+    TodoItem.belongsTo(models.Todo, {
+      as: 'todo',
+      foreignKey: 'todoId'
+    });
+  };
+  return TodoItem;
+};
