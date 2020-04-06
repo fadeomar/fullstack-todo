5e79b996b2c2a33cf9c1e772462c1d3ee0678a78
fadeomar <fadeomar2015@gmail.com>
2020-04-06 03:59:35 +0300

    create the landing component

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../component/Nav';
+import Landing from '../component/Landing';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Landing />
     </div>
   );
 }
