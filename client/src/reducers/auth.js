8405bf9af9287a535884085d41f91e3438f8a193
fadeomar <fadeomar2015@gmail.com>
2020-04-08 03:46:59 +0300

    add SIGNUP_USER_SUCESS case to the reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,11 +1,8 @@
-const initialState = {
-  user: null,
-  isAuthenticated: false,
-};
+import { initialState } from "../context/authContext";
 
 const authReducer = (state = initialState , action) => {
   switch (action.type){
-    case 'SIGNUP_USER':
+    case 'SIGNUP_USER_SUCESS':
       return {...state, user: action.payload, isAuthenticated: true};
     case 'SIGNIN_USER':
       return {...state, user: action.payload, isAuthenticated: true};
