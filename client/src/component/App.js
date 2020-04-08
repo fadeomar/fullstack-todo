c37e2e4ebe3cb2788fb524591a4c80d8e4fa6f42
fadeomar <fadeomar2015@gmail.com>
2020-04-08 19:51:35 +0300

    use context to get signup action

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -5,13 +5,16 @@ import { Provider, Context } from '../context/authContext';
 import { Route, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
-  const all = useContext(Context)
-  console.log(all)
+  const { signUp } = useContext(Context)
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={Landing}/>
-        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signup" render={() => {
+          return <SignUp signUp={async (user) => {
+            await signUp(user)
+          }} />
+        }} />
       </Router>
     </div>
   );
