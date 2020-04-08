5882b5c384c6ee9d864d51b9ead7f7759bcfb128
fadeomar <fadeomar2015@gmail.com>
2020-04-08 19:13:19 +0300

    add handleSubmit function

--- client/src/component/auth/SignUp.js
+++ client/src/component/auth/SignUp.js
@@ -15,32 +15,36 @@ const SignUp = () => {
   const [emailErr, setemailErr] = useState('');
   const [passwordErr, setpasswordErr] = useState('');
   const [confPasswordErr, setconfPasswordErr] = useState('');
-  const handleSignUp = () => {};
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (name && email && password && confPassword && !nameErr && !emailErr && !passwordErr && !confPasswordErr) {
+      await signUp({ name, email, password });
+    }
+  }
   const handleChange = (e) => {
-    console.log('sssssss', e.target.name)
-    const user = {};
     // email adress regx
     const emailRegEx = RegExp(
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
     const name = e.target.name;
+    const value = e.target.value;
     // validations
     switch (name) {
       case 'name':
-        setname(user.name);
-        user.name.length < 3 ? setnameErr('Name must be at least 3 characters!') : setnameErr('');
+        setname(value);
+        value.length < 3 ? setnameErr('Name must be at least 3 characters!') : setnameErr('');
         break;
       case 'email':
-        setemail(user.email);
-        !emailRegEx.test(user.email) ? setemailErr('Invalid Email!') : setemailErr('');
+        setemail(value);
+        !emailRegEx.test(value) ? setemailErr('Invalid Email!') : setemailErr('');
         break;
       case 'password':
-        setpassword(user.password);
-        user.password.length < 8 ? setpasswordErr('Password must be at least 8 characters!') : setpasswordErr('');
+        setpassword(value);
+        value.length < 8 ? setpasswordErr('Password must be at least 8 characters!') : setpasswordErr('');
         break;
       case 'confPassword':
-        setconfPassword(user.confPassword);
-        user.confPassword !== password ? setconfPasswordErr('Passwords do not match!') : setconfPasswordErr('');
+        setconfPassword(value);
+        value !== password ? setconfPasswordErr('Passwords do not match!') : setconfPasswordErr('');
         break;
       default:
         break;
@@ -51,10 +55,13 @@ const SignUp = () => {
   return (
     <Layout>
       <AuthFormWrapper>
-      <h2 className="text-center">Create an Account</h2>
-      <form className="mt-4" onSubmit={handleSignUp}>
-        <div className="form-group">
-        <label htmlFor="name">Enter Full Name</label>
+        <h2 className="text-center">Create an Account</h2>
+        {state.signUpErr && <div className="alert alert-danger text-center">
+          <span className="text-danger text-capitalize">{state.signUpErr}</span>
+          </div>}
+        <form className="mt-4" onSubmit={handleSignUp}>
+          <div className="form-group">
+            <label htmlFor="name">Enter Full Name</label>
             <input
               type="text"
               name="name"
@@ -66,10 +73,56 @@ const SignUp = () => {
               placeholder="Full Name"
               onChange={(e) => handleChange(e, 'name')}
             />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Sign Up"/>
-      </form>
-      <p className="float-left">
+            {nameErr && <small className="text-danger">{nameErr}</small>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Enter Email Address</label>
+            <input
+              type="email"
+              name="email"
+              className={classnames(
+                "form-control",
+                { 'is-invalid': emailErr, 'is-valid': !emailErr && email.length }
+              )}
+              id="email"
+              placeholder="Email Address"
+              onChange={(e) => handleChange(e, 'email')}
+            />
+            {emailErr && <small className="text-danger">{emailErr}</small>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Enter Password</label>
+            <input
+              type="password"
+              name="password"
+              className={classnames(
+                "form-control",
+                { 'is-invalid': passwordErr, 'is-valid': !passwordErr && password.length }
+              )}
+              id="password"
+              placeholder="Password"
+              onChange={(e) => handleChange(e, 'password')}
+            />
+            {passwordErr && <small className="text-danger">{passwordErr}</small>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="Confirm Password">Confirm Password</label>
+            <input
+              type="password" 
+              name="confPassword"
+              className={classnames(
+                "form-control",
+                { 'is-invalid': confPasswordErr, 'is-valid': !confPasswordErr && confPassword.length }
+              )}
+              id="confPassword"
+              placeholder="confPassword"
+              onChange={(e) => handleChange(e, 'confPassword')}
+            />
+            {confPasswordErr && <small className="text-danger">{confPasswordErr}</small>}
+          </div>
+          <input type="submit" className="btn btn-primary" value="Sign Up"/>
+        </form>
+        <p className="float-left">
           Already have an account? <NavLink to="signin">Sign In</NavLink>
         </p>
       </AuthFormWrapper>
