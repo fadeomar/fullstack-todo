091d1767659ba2c6a948b19647f67d4394a891d0
fadeomar <fadeomar2015@gmail.com>
2020-04-09 00:49:06 +0300

    create reducer and actions and context fro todos

--- client/src/component/todos/ListTodos.js
+++ client/src/component/todos/ListTodos.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../Layout/Layout';
+import { TodoList } from './Styles';
+import Model from './Model';
 
 
 const ListTodos = () => {
+  const [showModal, setshowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setshowModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setshowModal(false);
+  }
   return (
     <Layout>
+            <TodoList>
+        {showModal && <Model closeModal={handleCloseModal} />}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 header text-center mt-3">
+              <h1>My Todos</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <button className="btn btn-primary float-right" onClick={handleShowModal}>New</button>
+            </div>
+          </div>
+        </div>
+      </TodoList>
 
     </Layout>
   )
