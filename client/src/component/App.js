561576c10e7069451771c84f97e28f3f86980c53
fadeomar <fadeomar2015@gmail.com>
2020-04-08 18:54:45 +0300

    user router dom in the app

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react';
 import Landing from '../component/Landing';
+import SignUp from './auth/SignUp';
 import { Provider, Context } from '../context/authContext';
+import { Route, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   const all = useContext(Context)
   console.log(all)
   return (
     <div className="App">
-      <Landing />
+      <Router>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/signup" component={SignUp} />
+      </Router>
     </div>
   );
 }
